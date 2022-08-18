511963535cd96b4b698cde8f48f9a8098317d801
hasem <hasemspi@gmail.com>
2022-08-18 23:58:31 +0600

    update data

---
+++ js/case.js
@@ -0,0 +1,30 @@
+function getcaseCart(IsValue) {
+    const getvalue = document.getElementById('get_value');
+    const getData = getvalue.value;
+    const getDataString = parseInt(getData);
+    let getFinalValue;
+    if (IsValue == true) {
+        getFinalValue = getDataString + 1;
+    } else {
+        getFinalValue = getDataString - 1;
+    }
+    getvalue.value = getFinalValue;
+    return getFinalValue;
+}
+
+function getotalPrieceValue(getFinalValue) {
+
+    const getFinalValuese = getFinalValue * 59;
+    const getPrince = document.getElementById('price');
+    getPrince.innerText = getFinalValuese;
+}
+
+document.getElementById('Case_Iphone').addEventListener('click', function () {
+    const getFinalValue = getcaseCart(true);
+    getotalPrieceValue(getFinalValue);
+})
+
+document.getElementById('get_minus').addEventListener('click', function () {
+    const getFinalValue = getcaseCart(false);
+    getotalPrieceValue(getFinalValue);
+})
---
+++ js/iphone.js
@@ -0,0 +1,30 @@
+function getiPhoneData(IsIncre) {
+    const getValue = document.getElementById('getIphoneValue');
+    const getData = getValue.value;
+    const getDataString = parseInt(getData);
+    let newCurrentData;
+    if (IsIncre == true) {
+        newCurrentData = getDataString + 1;
+    } else {
+        newCurrentData = getDataString - 1;
+    }
+    getValue.value = newCurrentData;
+    return newCurrentData;
+}
+
+function getdataTotalPrice(newCurrentData) {
+    const getPrice = document.getElementById('getPrice');
+    const getprices = newCurrentData * 1219;
+    getPrice.innerText = getprices;
+}
+
+
+document.getElementById('getIphoneIncre').addEventListener('click', function () {
+    const newCurrentData = getiPhoneData(true);
+    getdataTotalPrice(newCurrentData);
+
+})
+document.getElementById('getIphoneDec').addEventListener('click', function () {
+    const newCurrentData = getiPhoneData(false);
+    getdataTotalPrice(newCurrentData);
+})
